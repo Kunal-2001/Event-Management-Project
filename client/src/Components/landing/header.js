bb9e0744d31c1c11ae1b5512e834694b7bd44bcf
Kunal-2001 <kunal999946@gmail.com>
2021-06-01 19:44:21 +0530

    Commit

--- client/src/Components/landing/header.js
+++ client/src/Components/landing/header.js
@@ -1,44 +1,41 @@
 import React from "react";
 import ContactSupportOutlinedIcon from "@material-ui/icons/ContactSupportOutlined";
 import LocalOfferOutlinedIcon from "@material-ui/icons/LocalOfferOutlined";
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
   return (
-    <Router>
-      <div className="header-container">
-        <ul className="header">
-          <li>
-            <h1>Event Finder</h1>
+    <div className="header-container">
+      <ul className="header">
+        <li>
+          <h1>Event Finder</h1>
+        </li>
+        <div className="right">
+          <li className="More-info">
+            <Link data-testid="contact" to="/" className="header-elements">
+              Contact Us <ContactSupportOutlinedIcon />
+            </Link>
+            <Link
+              data-testid="pricing"
+              to="/pricing"
+              className="header-elements"
+            >
+              Pricing <LocalOfferOutlinedIcon />
+            </Link>
+            <Link data-testid="login" to="/login" className="header-elements">
+              Login
+            </Link>
+            <Link
+              data-testid="register"
+              to="/register"
+              className="header-elements"
+            >
+              Register
+            </Link>
           </li>
-          <div className="right">
-            <li className="More-info">
-              <Link data-testid="contact" to="/" className="header-elements">
-                Contact Us <ContactSupportOutlinedIcon />
-              </Link>
-              <Link
-                data-testid="pricing"
-                to="/pricing"
-                className="header-elements"
-              >
-                Pricing <LocalOfferOutlinedIcon />
-              </Link>
-              <Link data-testid="login" to="/login" className="header-elements">
-                Login
-              </Link>
-              <Link
-                data-testid="register"
-                to="/register"
-                className="header-elements"
-              >
-                Register
-              </Link>
-            </li>
-          </div>
-        </ul>
-      </div>
-      {/* //{" "} */}
-    </Router>
+        </div>
+      </ul>
+    </div>
   );
 }
